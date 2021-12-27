be684bf551a538ce0feb385fbbd951961e03278b
Daniel Jecu <catalin.jecu@gmail.com>
2021-12-27 11:48:25 +0200

    add interfaces and services for tasklist, task

---
+++ ui/src/services/taskService.ts
@@ -0,0 +1,15 @@
+import { AxiosResponse } from "axios";
+import axiosInstance from "../utils/axiosInstance";
+
+import { ITaskDetails } from '../interfaces';
+
+const getTasks = (): Promise<AxiosResponse<ITaskDetails[]>> => {
+  return axiosInstance.get<ITaskDetails[]>('/task');
+};
+
+
+const TaskService = {
+  getTasks
+};
+
+export default TaskService;
