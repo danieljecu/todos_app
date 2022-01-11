528d10d64b0206e7bc9b7eaf724cc2533d54061f
Daniel Jecu <catalin.jecu@gmail.com>
2022-01-11 18:39:32 +0000

    tidy up

--- ui/src/routers/app.router.tsx
+++ ui/src/routers/app.router.tsx
@@ -4,12 +4,8 @@ import React from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import { NAVIGATION_ROUTES } from "../constants/navigation";
 
-interface UserStatusProps {
-    auth?: boolean;
-  }
 
-//TODO Header will be replaced with <LogedInHeader/>  
-const AppRouter: React.FC<UserStatusProps> = () => <BrowserRouter>
+const AppRouter: React.FC = () => <BrowserRouter>
     <Header />
     <Routes>
         <Route path={NAVIGATION_ROUTES.HOME} element={<Home/>}/>
