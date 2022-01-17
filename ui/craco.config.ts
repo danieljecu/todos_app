fb9f39f7438d6cab592725ce91354b21c936e0bd
Daniel Jecu <catalin.jecu@gmail.com>
2022-01-17 10:49:02 +0000

    add craco and craco-alias, working: no trailing commas, no comments inside json

---
+++ ui/craco.config.ts
@@ -0,0 +1,16 @@
+const CracoAlias = require("craco-alias");
+
+export const plugins = [
+  {
+    plugin: CracoAlias,
+    options: {
+      // see in examples section
+      source: "tsconfig",
+      //baseUrl SHOULD be specified
+      // plugin does not take it from tsconfig
+      baseUrl: "./src",
+      // tsConfigPath should point to the file where "baseUrl" and "paths" are specified
+      tsConfigPath: "./tsconfig.paths.json"
+    }
+  }
+];
