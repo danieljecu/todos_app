76e845e1967760d337b29d20814a2ef42f537978
Daniel Jecu <catalin.jecu@gmail.com>
2022-01-19 20:20:38 +0000

    tie task add remove

--- ui/src/pages/Home/TasksView/useTasklistTasks.tsx
+++ ui/src/pages/Home/TasksView/useTasklistTasks.tsx
@@ -2,28 +2,33 @@ import { useEffect, useState } from "react";
 import { ITaskDetails } from "interfaces";
 import dummyProjects from "../../../dummydata/dummy";
 import { DropResult, ResponderProvided } from "react-beautiful-dnd";
-import { ProjectService, TasklistService, TaskService } from "services";
-
+import { TaskService } from "services";
 
 export const useTasklistTasks = (tasklistId: number) => {
   const [tasks, setTasks] = useState<any[]>([]);
   const retrieveTasklist = async () => {
-
     const tasksResponse = await TaskService.getTasksByTasklistId(tasklistId);
     setTasks(tasksResponse.data);
-    console.log("task;",tasksResponse.data);
+    console.log("task;", tasksResponse.data);
   };
-  
 
   useEffect(() => {
     retrieveTasklist().catch(console.log);
   }, []);
 
-  const addTask = (task: ITaskDetails) => {
-    if (tasks) {
-      setTasks((prevState) => [...prevState, task]);
-    }
+  const addTask = (newTask: ITaskDetails) => {
+    TaskService.createTask(tasklistId, newTask)
+      .then(retrieveTasklist)
+      .catch(console.log);
+
+    // setTasks((prevState) => [...prevState, newTask]);
   };
+
+  const removeTaskById = (taskId: number) => {
+    TaskService.deleteTaskById(taskId)
+      .then(retrieveTasklist)
+      .catch(console.log);
+  }
 
   // const updateTask = (task: ITaskDetails) => {
   //   if (tasks) {
@@ -31,13 +36,9 @@ export const useTasklistTasks = (tasklistId: number) => {
   //       prevState.map((t) => (t.id === task.id ? task : t))
   //     );
   //   }
-  // }; 
+  // };
 
-  const removeTaskById = (taskId: number) => {
-    if (tasks) {
-      setTasks(tasks.filter((t) => t.id !== taskId));
-    }
-  };
+ 
 
-  return { tasks, setTasks, addTask, removeTaskById};
+  return { tasks, setTasks, addTask, removeTaskById };
 };
--- ui/src/services/taskService.ts
+++ ui/src/services/taskService.ts
@@ -11,9 +11,25 @@ const getTasksByTasklistId = (tasklistId: number): Promise<AxiosResponse<ITaskDe
   return axiosInstance.get<ITaskDetails[]>(`/task?tasklist=${tasklistId}`);
 };
 
+const createTask = (tasklistId: number, body: ITaskDetails): Promise<AxiosResponse<ITaskDetails>> => {
+  return axiosInstance.post<ITaskDetails>(`/task?tasklist=${tasklistId}`, body);
+};
+
+const deleteTaskById = (taskId: number): Promise<AxiosResponse<ITaskDetails>> => {
+  return axiosInstance.delete<ITaskDetails>(`/task/${taskId}`);
+};
+
+const updateTaskById = (taskId: number, body: ITaskDetails): Promise<AxiosResponse<ITaskDetails>> => {
+  return axiosInstance.put<ITaskDetails>(`/task/${taskId}`, body);
+};
+
+
 const TaskService = {
   getTasksByTasklistId,
   getTasks,
+  createTask,
+  deleteTaskById,
+  updateTaskById
 };
 
 export default TaskService;
