fbe0510cc23e4e93018fee4c10e09f94ca749b01
Daniel Jecu <catalin.jecu@gmail.com>
2022-01-26 11:40:37 +0000

    add login, register, user services

--- ui/src/services/userService.ts
+++ ui/src/services/userService.ts
@@ -1,15 +1,49 @@
 import { AxiosResponse } from "axios";
 import axiosInstance from "../utils/axiosInstance";
-
-import { IUserDetails } from '../interfaces';
+import { IUserDetails } from "../interfaces";
 
 const getUsers = (): Promise<AxiosResponse<IUserDetails[]>> => {
-  return axiosInstance.get<IUserDetails[]>('/user');
+  return axiosInstance.get<IUserDetails[]>("/user");
+};
+
+interface UserCredentials {
+  email: string;
+  password: string;
+}
+
+interface IUserSession {
+  user: IUserDetails;
+  accessToken?: string;
+  expiresIn?: number;
+  // tokenType?: string;
+  // refreshToken?: string;
+  // roles?: [];
+}
+
+const login = ({
+  email,
+  password,
+}: UserCredentials): Promise<AxiosResponse<IUserSession>> => {
+  return axiosInstance.post<IUserSession>("/login", {
+    email,
+    password,
+  });
+};
+
+const register = ({
+  email,
+  password,
+}: UserCredentials): Promise<AxiosResponse<IUserSession>> => {
+  return axiosInstance.post<IUserSession>("/register", {
+    email,
+    password,
+  });
 };
 
 const UserService = {
-  getUsers
+  getUsers,
+  login,
+  register,
 };
-
 
 export default UserService;
