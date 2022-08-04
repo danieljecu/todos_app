b6b5d80d9fcbf79abf4ba451575c9b5b8e2720ca
Daniel Jecu <catalin.jecu@gmail.com>
2022-08-04 23:39:07 +0300

    update layout comments

--- ui/src/components/layout/index.tsx
+++ ui/src/components/layout/index.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import { HeaderBar } from "components/Header/styles";
+import { PageContainer } from "./page-container";
 
+// NOT USED YET, the ideea is to have a generic page container that can be used for all pages
+// not sure is this is the best way to do this
 function Layout() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div>
+      <HeaderBar></HeaderBar>
+      <PageContainer>
+        here is the acctual content, maybe you want it inset by default
+      </PageContainer>
+    </div>
+  );
 }
 
 export default Layout;
