44fe019803d3c73bdd098ca4669a3af4b1274009
Catalin Daniel Jecu <daniel.jecu@unosquare.com>
2022-08-08 19:47:27 +0300

    add winston and install morgan

--- api/index.ts
+++ api/index.ts
@@ -7,6 +7,8 @@ import YAML from "yamljs";
 import { UserRouter, ProjectRouter, TaskRouter, AuthRouter } from "./routers";
 import cors from "cors";
 import { verifyToken } from "./middlewares/checkJwt";
+import Logger from "./utils/logger";
+
 
 dotenv.config();
 
@@ -21,6 +23,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const swaggerDocument = YAML.load("./basic-api-doc.yml");
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+app.get("/logger", (_, res) => {
+    Logger.error("This is an error log");
+    Logger.warn("This is a warn log");
+    Logger.info("This is a info log");
+    Logger.http("This is a http log");
+    Logger.debug("This is a debug log");
+  
+    res.send("Hello world");
+  });
+  
+
 app.use("/", AuthRouter);
 app.use("/user", UserRouter, verifyToken);
 app.use("/project", ProjectRouter, verifyToken);
@@ -28,3 +41,4 @@ app.use("/project", ProjectRouter, verifyToken);
 app.use("/task", TaskRouter, verifyToken);
 
 app.listen(PORT, () => console.log(`Running on ${PORT} ⚡`));
+
--- api/utils/index.ts
+++ api/utils/index.ts
@@ -1,6 +1,7 @@
 import { prisma } from "./db_client";
+import Logger from "./logger"
 
 // More hooks/utils will be added in the future and this could
 // lead to confusing imports if there is a default
 // eslint-disable-next-line import/prefer-default-export
-export { prisma };
+export { prisma, Logger };
