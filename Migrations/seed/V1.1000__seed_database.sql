fe6374d99baa0350c4ec23f8ade1ec9ea0d009cd
Daniel Jecu <catalin.jecu@gmail.com>
2022-08-10 20:59:58 +0300

    update seed

--- Migrations/seed/V1.1000__seed_database.sql
+++ Migrations/seed/V1.1000__seed_database.sql
@@ -1,28 +1,58 @@
-INSERT INTO users (id, email, username, password, first_name, last_name)
-VALUES (0, 'daniel@u.com', 'danielj', '999','Catalin Daniel','Jecu');
-
-
+INSERT INTO users (
+        id,
+        email,
+        username,
+        password,
+        first_name,
+        last_name
+    )
+VALUES (
+        0,
+        'daniel@u.com',
+        'danielj',
+        '$2a$10$dRvnMXph.QIEk3rRq28Ks.gfUFyftPhtkEKSxizHPstg2OU1M2FFK',
+        'Catalin Daniel',
+        'Jecu'
+    );
 INSERT INTO projects (id, name)
 VALUES (0, 'Personal'),
-       (1, 'Work Project');
-
+    (1, 'Work Project');
 INSERT INTO project_users (id, project_id, user_id)
 VALUES (0, 0, 0),
-       (1, 1, 0);
-
-
+    (1, 1, 0);
 INSERT INTO task_lists (id, name, project_id)
 VALUES (0, 'DB tasks', 1),
-       (1, 'API tasks', 1),
-       (2, 'FE tasks', 1),
-       (3, 'Home tasks', 0),
-       (4, 'Chores tasks', 0);
-
-INSERT INTO tasks (id, title, description, task_list_id, task_status_id)
-VALUES (0, 'erd database', 'Ladies Gaelic Football', 0, 1);
-
-
-INSERT INTO tasks (id, title, description, task_list_id, task_status_id)
-VALUES (1, 'router validation', 'Ladies Gaelic Football',1,  2 ),
-       (2, 'api', 'Ladies Gaelic Football', 1, 1),
-       (3, 'frontend', 'Ladies Gaelic Football', 2, 1);
+    (1, 'API tasks', 1),
+    (2, 'FE tasks', 1),
+    (3, 'Home tasks', 0),
+    (4, 'Chores tasks', 0);
+INSERT INTO tasks (
+        id,
+        title,
+        description,
+        task_list_id,
+        task_status_id
+    )
+VALUES (
+        0,
+        'erd database',
+        'Ladies Gaelic Football',
+        0,
+        1
+    );
+INSERT INTO tasks (
+        id,
+        title,
+        description,
+        task_list_id,
+        task_status_id
+    )
+VALUES (
+        1,
+        'router validation',
+        'Ladies Gaelic Football',
+        1,
+        2
+    ),
+    (2, 'api', 'Ladies Gaelic Football', 1, 1),
+    (3, 'frontend', 'Ladies Gaelic Football', 2, 1);
