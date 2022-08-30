bfdec1899a84f2dd80131d1c1836864c61dc68cb
Daniel Jecu <catalin.jecu@gmail.com>
2022-08-30 16:11:49 +0300

    start testing

--- ui/src/App.test.tsx
+++ ui/src/App.test.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+// test register
+// test login
+
+// test create a project_id
+// test create a tasklist
+// test create a task
+// edit a task
+/////test edit a task_id description or status
+/////test edit a task_id task_list_id
+// delete a task
+// delete a tasklist
+// delete a project_id
+
+test("renders learn react link", () => {
   render(<App />);
+
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
